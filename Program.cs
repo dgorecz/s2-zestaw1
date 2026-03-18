4a7f8cc7ed96758a531342dea79155c86f5adb16
dgorecz <dawidgorecny@gmail.com>
2026-03-18 08:38:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace s2_zestaw1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*----------------------------------------------------
+                                Zadanie 1.
+            ----------------------------------------------------*/
+            
+            List<int> lista = new List<int>() {3, 7, 2, 9, 1, 5, 8, 6 };
+            foreach (int i in lista)
+            { Console.WriteLine(i); }
+
+            lista.Sort();
+
+            lista.RemoveAll(x => x < 5);
+            
+            lista.AddRange(new int[] { 10, 11, 12, });
+
+            var listaSuma = lista.Sum();
+            var listaSrednia = lista.Average();
+
+            Console.WriteLine($"\nSuma = {listaSuma}" +
+                $"\nŚrednia = {listaSrednia}");
+        }
+    }
+}
